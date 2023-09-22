e18cf6c060e97a739778b60d16342c636f9e1043
VictorScar <Babooner@mail.ru>
2023-09-22 13:40:29 +0400

    fix_jump_logic

--- Assets/Scripts/Level/Characters/PlayerController.cs
+++ Assets/Scripts/Level/Characters/PlayerController.cs
@@ -12,14 +12,18 @@ public class PlayerController : MonoBehaviour, IPunObservable, IOnEventCallback
     [SerializeField] private PhotonView photonView;
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Gun gun;
+    [SerializeField] private GroundCheker groundChecker;
 
     [SerializeField] private float moveSpeed = 100f;
     [SerializeField] private float jumpForce = 2f;
+    [SerializeField] private float jumpDuration = 1f;
 
+    [SerializeField] private float gravityForce = 200f;
     [SerializeField] private float maxHealth = 100f;
     [SerializeField] private float health = 100f;
 
     private PointCounter pointCounter;
+   // private bool canJumping = false;
 
     public PointCounter PointCounter { get => pointCounter; }
 
@@ -31,14 +35,18 @@ public class PlayerController : MonoBehaviour, IPunObservable, IOnEventCallback
     public void Move(Vector2 direction)
     {
         PlayerRotate(direction);
-        //rb.velocity = direction * moveSpeed * Time.fixedDeltaTime;
-        transform.position += new Vector3(direction.x, direction.y, 0) * moveSpeed * Time.deltaTime;
+        rb.velocity = direction * moveSpeed * 100f * Time.fixedDeltaTime;
+        //transform.position += new Vector3(direction.x, direction.y, 0) * moveSpeed * Time.deltaTime;
     }
 
     public void Jump()
     {
-        //StartCoroutine(JumpMoving());
-        rb.AddForce(Vector2.up * jumpForce);
+        if (groundChecker.IsGrounded)
+        {
+            StartCoroutine(JumpMoving());
+
+        }
+
     }
 
     public void Fire()
@@ -48,17 +56,17 @@ public class PlayerController : MonoBehaviour, IPunObservable, IOnEventCallback
 
     public void GetDamage(float damage)
     {
-        if(photonView.IsMine)
+        if (photonView.IsMine)
 
-        if (damage <= health)
-        {
-            health -= damage;
-        }
-        else
-        {
-            health = 0f;
-            Death();
-        }
+            if (damage <= health)
+            {
+                health -= damage;
+            }
+            else
+            {
+                health = 0f;
+                Death();
+            }
     }
 
     private void Death()
@@ -81,14 +89,24 @@ public class PlayerController : MonoBehaviour, IPunObservable, IOnEventCallback
 
     IEnumerator JumpMoving()
     {
-        float currentHeight = 0f;
+        //float currentHeight = 0f;
 
-        while (currentHeight < jumpForce)
+        //while (currentHeight < jumpForce)
+        //{
+        //    Vector3.MoveTowards(transform.position, transform.position + new Vector3(0, jumpForce, 0), 0.5f);
+        //    currentHeight += 0.5f;
+        //    yield return null;
+        //}
+        float currentTime = 0;
+       // canJumping = false;
+        while (currentTime < jumpDuration)
         {
-            Vector3.MoveTowards(transform.position, transform.position + new Vector3(0, jumpForce, 0), 0.5f);
-            currentHeight += 0.5f;
+            rb.AddForce(Vector2.up * jumpForce * Time.fixedDeltaTime, ForceMode2D.Impulse);
+            currentTime += Time.fixedDeltaTime;
             yield return null;
         }
+        //yield return new WaitForSeconds(jumpDuration);
+        //canJumping = true;
 
     }
 
@@ -110,13 +128,31 @@ public class PlayerController : MonoBehaviour, IPunObservable, IOnEventCallback
         throw new NotImplementedException();
     }
 
+    private void FixedUpdate()
+    {
+        rb.AddForce(Vector2.down * gravityForce * Time.fixedDeltaTime);
+    }
+
     private void OnEnable()
     {
-       
+
     }
 
     private void OnDisable()
     {
-        
+
     }
+
+    //private void OnTriggerStay2D(Collider2D collision)
+    //{
+    //    if (collision.IsTouchingLayers(LayerMask.NameToLayer("Ground")))
+    //    {
+    //        canJumping = true;
+    //    }
+    //    else
+    //    {
+    //        canJumping = false;
+    //    }
+    //}
+      
 }
